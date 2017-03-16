ad7996bd7873eb345cfe9cdb3b5444087de21179
Paul Andree M. Vergara <paul.vergara05@gmail.com>
2017-03-16 12:27:23 +0800

    Index and app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 (function() {
   var app = angular.module('resto', []);
 
+  app.controller('TestAngular', function(){
+      this.names = test;
   });
 
+  var test = [
+    { name: 'Paul', test: 1 },
+    { name: 'Andree', test: 2 },
+    { name: 'Vergara', test: 3 }
   ];
 })();
