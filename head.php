754f3ba04a85c4ac0c4cc442ae8debb7e4760e77
Paul Andree M. Vergara <paul.vergara05@gmail.com>
2017-03-16 12:27:09 +0800

    Backbone

---
+++ head.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>ERP</title>
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+        
+        <!-- Individual Team member CSS -->
+        <link rel="stylesheet" href="css/style_alec.css">
+        <link rel="stylesheet" href="css/style_andrew.css">
+        <link rel="stylesheet" href="css/style_godfrey.css">
+        <link rel="stylesheet" href="css/style_ja.css">
+        <link rel="stylesheet" href="css/style_jae.css">
+        <link rel="stylesheet" href="css/style_paul.css">
+        <link rel="stylesheet" href="css/style_precious.css">
