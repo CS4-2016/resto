e8ca855f43a66d95778338a912f22096f0ccb3f4
jaeangelopb <jaeangelob@gmail.com>
2017-03-24 19:27:48 +0800

    Register

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-    require_once("dbconn.php");
+    session_start();
+    if(!empty($_SESSION['username'])){
+        unset($_SESSION['username']);
+    }
 ?>
 
 <!DOCTYPE html>
 <html ng-app="resto">
     <head>
-        <?php require_once("head.php"); ?>        
-        <link href="css/style_paul.css" rel="stylesheet">
-        <link href="css/style_jae.css" rel="stylesheet">
+        <?php require_once("head.php"); ?>    
+        <link rel="stylesheet" href="css/style_jae.css">
+        <link rel="stylesheet" href="css/style_paul.css">
     </head>
     <body>
     
@@ -16,18 +19,29 @@
             <div class="login-wrapper">
                 <center>
                     <div class="login-header">
-                        <img src="img/el-banner.png" class="el-img login-banner unselectable">
+                        <img src="img/el-banner.png" class="el-img login-banner">
                     </div>
                 </center>
                 <center>
                     <div class="container-resto">    
-                    <center>
-                        <label style="margin-top: 30px;" for="username"><i class="fa fa-user"></i> Username</label>
-                        <input class="login-text-box" type="text" id="username" name="username">
-                        <label style="margin-top: 15px;"for="password"><i class="fa fa-key"></i> Password</label>
-                        <input class="login-text-box login-password-text-box" type="password" id="password" name="password"><i class="fa fa-eye show-off" id="showPassword"></i>
-                    </center>
-                    <center><input type="submit" value="LOGIN" class="btn-login"></center>
+                        <form method="post" action="login-authentication.php">
+                            <center>
+                                <label style="margin-top: 30px;" for="username"><i class="fa fa-user"></i> Username</label>
+                                <input class="login-text-box" type="text" id="username" name="username" <?php if(!empty($_SESSION['login-attempt']) && $_SESSION['login-attempt'] == 'username'){ echo "value='".$_SESSION['username-attempt']."'"; }?>>
+                                <label style="margin-top: 15px;" for="password"><i class="fa fa-key"></i> Password</label>
+                                <input class="login-text-box login-password-text-box" type="password" id="password" name="password"><i class="fa fa-eye show-off" id="showPassword"></i>
+                                <?php if(!empty($_SESSION['login-attempt']) && $_SESSION['login-attempt'] == 'username'){ ?>
+                                    <span class="login-error">Password is incorrect.</span>
+    <?php 
+                                    unset($_SESSION['login-attempt']);
+                                    unset($_SESSION['username-attempt']);
+    } ?>
+                            </center>
+                            <center>
+                                <input type="submit" value="LOGIN" class="btn-login">
+                                <a href="register.php"><input type="button" value="REGISTER" class="btn-register"></a>
+                            </center>
+                        </form>
                 </div>
                 </center>
                 
