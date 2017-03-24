9c622f9b3cb8028091d22ce8652dbcacf95e10fa
Andrew Veracruz <veracruzandrew@gmail.com>
2017-03-25 04:04:11 +0800

    forgot password 2
    updated (using plain mail )

--- fp2.php
+++ fp2.php
@@ -9,7 +9,7 @@
     {
         $dbcon=new PDO('mysql:host='.$GLOBALS['host'].';dbname='.$GLOBALS['dbase'], $GLOBALS['lin'], $GLOBALS['pwd']);
         $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $state=$dbcon->prepare("SELECT email FROM users WHERE email=?;");
+        $state=$dbcon->prepare("SELECT email, password FROM users WHERE email=?;");
         $state->execute(array($email));
         $row=$state->fetchAll();
     }
@@ -24,14 +24,15 @@
     }
     else
     {
-        $e=$row[0]['email'];
-        mail("veracruzandrew@gmail.com", "Testaeng", $e);
+        $p=$row[0]['password'];
+        mail("veracruzandrew@gmail.com", "Testaeng", $p);
+        header("Location: forgot-success.php");
         /*$mail = new PHPMailer;
         $mail->Sendmail = '/usr/sbin/sendmail';
         $mail->setFrom('veracruzandrew@gmail.com', 'Me');
         $mail->addAddress('veracruzandrew@gmail.com', 'This is ME');
         $mail->Subject  = 'First PHPMailer Message';
-        $mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer. Email is'.$email;
+        $mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer. Password is'.$p;
         if(!$mail->send()) {
           echo 'Message was not sent.';
           echo 'Mailer error: ' . $mail->ErrorInfo;
